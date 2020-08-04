84fcca0ca70c15054bc72cc6987935fe9dcbe285
heilili <2020961976@qq.com>
2020-08-04 08:10:28 +0800

    a

--- pro/untitled/src/pages/index.js
+++ pro/untitled/src/pages/index.js
@@ -1,70 +1,26 @@
 import React from "react";
-import XLSX from 'xlsx'
-import './config'
-import {
-    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
-} from 'recharts';
-function importExcel(file){
+import {LineChart,Line,CartesianGrid,XAxis,YAxis,Tooltip} from 'recharts'
 
-    // 获取上传的文件对象
-    const { files } = file.target;
-    // 通过FileReader对象读取文件
-    const fileReader = new FileReader();
-    fileReader.onload = event => {
-        try {
-            const { result } = event.target;
-            // 以二进制流方式读取得到整份excel表格对象
-            const workbook = XLSX.read(result, { type: 'binary' });
-            let data = []; // 存储获取到的数据
-            // 遍历每张工作表进行读取（这里默认只读取第一张表）
-            for (const sheet in workbook.Sheets) {
-                if (workbook.Sheets.hasOwnProperty(sheet)) {
-                    // 利用 sheet_to_json 方法将 excel 转成 json 数据
-                    data = data.concat(XLSX.utils.sheet_to_json(workbook.Sheets[sheet]));
-                    // break; // 如果只取第一张表，就取消注释这行
-                }
-            }
-            console.log(typeof(data));
-            global.yxx = data;
-            let a = JSON.stringify(data);
-            document.write(global.yxx);
-            console.log(a);
-        } catch (e) {
-            // 这里可以抛出文件类型错误不正确的相关提示
-            console.log('文件类型不正确');
-
-        }
-    };
-    // 以二进制方式打开文件
-    fileReader.readAsBinaryString(files[0]);
-
-}
+const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400},{name: 'Page B', uv: 300, pv: 2400, amt: 2400}];
 
 export default class Index extends React.Component{
     constructor(props){
-        console.log(global.yxx);
         super(props);
         this.state={
-
         }
     }
 
-
-
     render() {
+
         return(
-
             <body>
-            <input type='file' accept='.xlsx, .xls,.csv' onChange={(e)=>{importExcel(e)} }/>
-            <linechart data = {global.yxx}>
-                <XAxis dataKey="Country/Region"/>
-                <YAxis/>
+            <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                <XAxis dataKey="name" />
+                <YAxis />
                 <Tooltip />
-                <Line type="monotone" dataKey="Lat" stroke="#8884d8" activeDot={{ r: 8 }} />
-                <Line type="monotone" dataKey="Long" stroke="#82ca9d" />
-                <Legend />
-            </linechart>
-
+            </LineChart>
             </body>
         )
     }
